ad62a54ae9bf4c554e5917ddb4c74e82fdee0809
Mohamed <iammoaziiz@gmail.com>
2023-02-08 14:00:33 +0300

    Setup API in Property fragment

--- app/src/main/java/com/example/guryihii/feature_properties/presentation/property_list/PropertyFragment.kt
+++ app/src/main/java/com/example/guryihii/feature_properties/presentation/property_list/PropertyFragment.kt
@@ -1,11 +1,12 @@
 package com.example.guryihii.feature_properties.presentation.property_list
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.guryihii.R
+import androidx.fragment.app.viewModels
 import com.example.guryihii.databinding.FragmentPropertyBinding
 
 
@@ -13,18 +14,43 @@ class PropertyFragment : Fragment() {
     private var _binding: FragmentPropertyBinding? = null
     private val binding: FragmentPropertyBinding get() = _binding!!
 
-    private val viewModel
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private val viewModel: PropertyListViewModel by viewModels()
 
-    }
+    val state = viewModel.state.value
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_property, container, false)
+        _binding = FragmentPropertyBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupUI()
+    }
+
+    private fun setupUI() {
+        initViews()
+        initListeners()
+        observeViewState()
+    }
+
+    private fun initViews() {
+        TODO("Not yet implemented")
+    }
+
+    private fun initListeners() {
+        TODO("Not yet implemented")
+    }
+
+    private fun observeViewState() {
+        if(state.isLoading) {
+            Log.i("TAG", "observeViewState: Loading")
+        }
+
+        Log.i("TAG", "observeViewState: ${state.properties}")
     }
 
     companion object {
