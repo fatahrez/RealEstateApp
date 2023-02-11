009bfac45c98d684a918cf11fc33d1dcd57e5bb4
fatahrez <fertahrez@gmail.com>
2023-02-11 10:19:08 +0300

    feature: add New Project DTO and API service for new project

--- app/src/main/java/com/example/guryihii/feature_newProjects/data/remote/NewProjectAPI.kt
+++ app/src/main/java/com/example/guryihii/feature_newProjects/data/remote/NewProjectAPI.kt
@@ -1,4 +1,10 @@
 package com.example.guryihii.feature_newProjects.data.remote
 
+import com.example.guryihii.feature_newProjects.data.remote.dto.NewProjectDTO
+import retrofit2.http.GET
+
 interface NewProjectAPI {
+
+    @GET("properties/new_projects/all/")
+    fun getAllNewProjectsDTO(): List<NewProjectDTO>
 }
