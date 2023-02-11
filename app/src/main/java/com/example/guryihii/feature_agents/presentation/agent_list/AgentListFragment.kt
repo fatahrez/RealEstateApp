8700c72d3465d52c76e8ffee783de5108e78f9a6
fatahrez <fertahrez@gmail.com>
2023-02-11 14:37:24 +0300

    bug: add args to bundle to retrieve agent details

--- app/src/main/java/com/example/guryihii/feature_agents/presentation/agent_list/AgentListFragment.kt
+++ app/src/main/java/com/example/guryihii/feature_agents/presentation/agent_list/AgentListFragment.kt
@@ -79,6 +79,7 @@ class AgentListFragment : Fragment() {
 
     private fun navToAgentDetail(agent: Agent) {
         val bundle = Bundle()
+        bundle.putInt("agent_id", agent.id)
         view?.let {
             Navigation.findNavController(it)
                 .navigate(R.id.action_agentListFragment_to_agentDetailFragment, bundle)
