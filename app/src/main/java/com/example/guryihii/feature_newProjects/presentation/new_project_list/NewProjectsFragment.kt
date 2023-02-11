01dd74d7bb105ac9ce236042dd33c9da3388be86
fatahrez <fertahrez@gmail.com>
2023-02-11 12:47:24 +0300

    create new branch for details

--- app/src/main/java/com/example/guryihii/feature_newProjects/presentation/new_project_list/NewProjectsFragment.kt
+++ app/src/main/java/com/example/guryihii/feature_newProjects/presentation/new_project_list/NewProjectsFragment.kt
@@ -26,7 +26,6 @@ class NewProjectsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout for this fragment
         _binding = FragmentNewProjectsBinding.inflate(inflater, container, false)
         return binding.root
     }
