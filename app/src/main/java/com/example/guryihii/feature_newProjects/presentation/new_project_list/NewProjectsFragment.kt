986ce1e70ce306bc09c571d0362c7f8ee06f4cb6
fatahrez <fertahrez@gmail.com>
2023-02-11 10:54:52 +0300

    feature: add binding and viewmodel to NewProjects fragmentl

--- app/src/main/java/com/example/guryihii/feature_newProjects/presentation/new_project_list/NewProjectsFragment.kt
+++ app/src/main/java/com/example/guryihii/feature_newProjects/presentation/new_project_list/NewProjectsFragment.kt
@@ -1,25 +1,52 @@
 package com.example.guryihii.feature_newProjects.presentation.new_project_list
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.example.guryihii.R
+import com.example.guryihii.databinding.FragmentNewProjectsBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
 
+@AndroidEntryPoint
 class NewProjectsFragment : Fragment() {
+    private var _binding: FragmentNewProjectsBinding? = null
+    private val binding: FragmentNewProjectsBinding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
+    private val viewModel: NewProjectListViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_projects, container, false)
+        _binding = FragmentNewProjectsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupUI()
+    }
+
+    private fun setupUI() {
+        observeViewState()
+    }
+
+    private fun observeViewState() {
+        lifecycleScope.launchWhenCreated {
+            viewModel.state.collect { state ->
+                if(state.isLoading) {
+                    Log.i("TAG", "observeViewState: loading...")
+                } else {
+                    Log.i("TAG", "observeViewState: ${state.newProjects}")
+                }
+            }
+        }
     }
 
     companion object {
