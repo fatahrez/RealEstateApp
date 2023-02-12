42eb29356f0d6abd14641f17f1d90c272b2b94eb
fatahrez <fertahrez@gmail.com>
2023-02-12 10:20:35 +0300

    add sign in state

---
+++ app/src/main/java/com/example/guryihii/feature_auth/presentation/sign_in/SignInState.kt
@@ -0,0 +1,8 @@
+package com.example.guryihii.feature_auth.presentation.sign_in
+
+import com.example.guryihii.feature_auth.domain.model.User
+
+data class SignInState(
+    val user: User? = null,
+    val isLoading: Boolean = false
+)
