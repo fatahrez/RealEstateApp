8f97bd1539bfb7edecef51d8004688d6ee762282
fatahrez <fertahrez@gmail.com>
2023-02-13 22:44:04 +0300

    add endpoint to remote profile service for getting profile data

--- app/src/main/java/com/example/guryihii/feature_profile/data/remote/ProfileAPI.kt
+++ app/src/main/java/com/example/guryihii/feature_profile/data/remote/ProfileAPI.kt
@@ -1,4 +1,11 @@
 package com.example.guryihii.feature_profile.data.remote
 
+import com.example.guryihii.feature_profile.data.remote.dto.ProfileDTO
+import retrofit2.http.GET
+
 interface ProfileAPI {
+
+    @GET("profile/me/")
+    suspend fun getProfile(): ProfileDTO
+
 }
