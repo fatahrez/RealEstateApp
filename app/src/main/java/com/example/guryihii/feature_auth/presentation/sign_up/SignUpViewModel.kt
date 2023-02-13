48b5d31516da8a45eeb9ef16a5b215c02787bcae
fatahrez <fertahrez@gmail.com>
2023-02-13 19:46:07 +0300

    add user sign up viewmodel to sign up presentation level

---
+++ app/src/main/java/com/example/guryihii/feature_auth/presentation/sign_up/SignUpViewModel.kt
@@ -0,0 +1,59 @@
+package com.example.guryihii.feature_auth.presentation.sign_up
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.guryihii.core.util.ResultWrapper
+import com.example.guryihii.feature_auth.domain.model.User
+import com.example.guryihii.feature_auth.domain.usecases.PostSignUpUser
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SignUpViewModel @Inject constructor(
+    private val postSignUpUser: PostSignUpUser
+): ViewModel() {
+
+    private val _state = MutableStateFlow(SignUpState())
+    private val state: StateFlow<SignUpState> get() = _state
+
+    fun signUpUser(user: User) {
+        viewModelScope.launch {
+            postSignUpUser(user)
+                .onEach { result ->
+                    when(result) {
+                        is ResultWrapper.Loading -> {
+                            _state.value = state.value.copy(
+                                isLoading = true
+                            )
+                        }
+                        is ResultWrapper.Success -> {
+                            _state.value = state.value.copy(
+                                isLoading = false,
+                                user = result.value
+                            )
+                        }
+                        is ResultWrapper.NetworkError -> {
+                            Log.e("TAG", "signUpUser: network error")
+                            _state.value = state.value.copy(
+                                isLoading = false,
+                                user = null
+                            )
+                        }
+                        is ResultWrapper.GenericError -> {
+                            Log.i("TAG", "signUpUser: ${result.error}")
+                            _state.value = state.value.copy(
+                                isLoading = false,
+                                user = null
+                            )
+                        }
+                    }
+                }.launchIn(viewModelScope)
+        }
+    }
+}
