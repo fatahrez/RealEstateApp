cecebf3290dc76df9a04ca7225a4fc9bdb27d6b1
fatahrez <fertahrez@gmail.com>
2023-02-13 22:30:09 +0300

    //profile: {

---
+++ app/src/main/java/com/example/guryihii/feature_profile/domain/repository/ProfileRepository.kt
@@ -0,0 +1,11 @@
+package com.example.guryihii.feature_profile.domain.repository
+
+import com.example.guryihii.core.util.ResultWrapper
+import com.example.guryihii.feature_profile.domain.model.Profile
+import kotlinx.coroutines.flow.Flow
+
+interface ProfileRepository {
+
+    suspend fun getProfile(): Flow<ResultWrapper<Profile>>
+
+}
