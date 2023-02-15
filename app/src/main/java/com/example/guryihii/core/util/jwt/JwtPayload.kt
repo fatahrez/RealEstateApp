5567f50fbefbeae3e4b43769383d67100d9b052f
fatahrez <fertahrez@gmail.com>
2023-02-15 18:33:57 +0300

    add token decoder class Jwt

--- app/src/main/java/com/example/guryihii/core/util/jwt/JwtPayload.kt
+++ app/src/main/java/com/example/guryihii/core/util/jwt/JwtPayload.kt
@@ -1,3 +1,15 @@
 package com.example.guryihii.core.util.jwt
 
-data class JwtPayload()
+import com.google.gson.annotations.SerializedName
+
+data class JwtPayload(
+    @SerializedName("token_type")
+    val tokenType: String,
+    val exp: Long,
+    val iat: Long,
+    val jti: String,
+    @SerializedName("user_id")
+    val userId: Int,
+    val role: String,
+    val username: String
+)
