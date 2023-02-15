ae1a24cc64ded8699b01a3badd77d7d17198a484
fatahrez <fertahrez@gmail.com>
2023-02-15 16:34:59 +0300

    authenticator is fired up

--- app/src/main/java/com/example/guryihii/core/shared/remote/TokenAuthenticator.kt
+++ app/src/main/java/com/example/guryihii/core/shared/remote/TokenAuthenticator.kt
@@ -4,11 +4,11 @@ import android.content.SharedPreferences
 import android.util.Log
 import androidx.core.content.edit
 import com.example.guryihii.core.util.Constants
-import com.example.guryihii.feature_auth.data.remote.AuthApiHolder
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
+import com.example.guryihii.core.util.ResultWrapper
+import com.example.guryihii.feature_refresh_token.data.repository.RefreshTokenRepositoryImpl
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
 import okhttp3.Authenticator
 import okhttp3.Request
 import okhttp3.Response
@@ -18,41 +18,46 @@ import javax.inject.Provider
 
 class TokenAuthenticator @Inject constructor(
     private val sharedPreferences: SharedPreferences,
-    private val authApiHolder: Provider<AuthApiHolder>,
-    private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
+    private val repositoryProvider: Provider<RefreshTokenRepositoryImpl>
 ): Authenticator {
     override fun authenticate(route: Route?, response: Response): Request? {
         return runBlocking {
             Log.i("TAG", "authenticate: here")
             Log.i("TAG", "authenticate: ${response.code}")
             if (response.code == 401) {
-                try {
-                    Log.i("TAG", "authenticate: here}")
-                    val token = getUpdatedToken()
-                    Log.i("TAG", "authenticate: token $token")
-                    sharedPreferences.edit {
-                        putString(Constants.ACCESS_TOKEN, token)
-                    }
-                    response.request.newBuilder().header("Authorization", "Bearer $token")
-                } catch (e: Exception) {
-                    Log.e("TAG", "authenticate: ", e)
+                val refreshToken = sharedPreferences.getString(Constants.REFRESH_TOKEN, "")
+                val updatedAccessToken = refreshToken?.let {
+                    refreshAccessToken(it)
+                        .onEach { result ->
+                            when (result) {
+                                is ResultWrapper.Success -> {
+                                    Log.i("TAG", "authenticate: ${result.value}")
+                                    result.value
+                                }
+                                is ResultWrapper.GenericError -> {
+                                    Log.i("TAG", "authenticate: ${result.error}")
+                                }
+                                is ResultWrapper.NetworkError -> {
+                                    Log.i("TAG", "Network Error")
+                                }
+                                is ResultWrapper.Loading -> {
+                                    Log.i("TAG", "Loading ... ")
+                                }
+                            }
+                        }
                 }
+                response.request.newBuilder()
+                    .header("Authorization", "Bearer $updatedAccessToken")
+                    .build()
             } else {
                 null
             }
-            null
         }
 
     }
 
-    private suspend fun getUpdatedToken(): String = withContext(ioDispatcher) {
-        Log.i("TAG", "getUpdatedToken: here again again")
-        val refreshToken = sharedPreferences.getString(Constants.REFRESH_TOKEN, null)
-        Log.i("TAG", "getUpdatedToken: $refreshToken")
-        if (refreshToken != null) {
-            authApiHolder.get().apiService().postRefreshAccessToken(refreshToken)
-        } else {
-            ""
-        }
+    private suspend fun refreshAccessToken(refreshToken: String): Flow<ResultWrapper<String>> {
+        val repository = repositoryProvider.get()
+        return repository.refreshToken(refreshToken)
     }
 }
