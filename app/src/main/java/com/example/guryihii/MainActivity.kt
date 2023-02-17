5e004dc427076e50ad0694754ec6bcc476640763
fatahrez <fertahrez@gmail.com>
2023-02-17 10:21:14 +0300

    successfully repopulate menu on login and logout

--- app/src/main/java/com/example/guryihii/MainActivity.kt
+++ app/src/main/java/com/example/guryihii/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
+import android.view.View
 import androidx.core.content.edit
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
@@ -35,7 +36,29 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
+        setContentView(view)
+        updateNavigation()
+    }
 
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
+        drawer.closeDrawer(GravityCompat.END)
+
+        when(item.itemId) {
+            R.id.sign_in -> {
+                navController.navigate(R.id.signInFragment, null)
+            }
+            R.id.sign_up -> {
+                navController.navigate(R.id.signUpFragment, null)
+            }
+            R.id.logout -> {
+                logout()
+            }
+        }
+        return true
+    }
+
+    fun updateNavigation() {
         val token = sharedPreferences.getString(Constants.ACCESS_TOKEN, null)
         if (token != null) {
             val tokenDecoder = Jwt(token)
@@ -56,13 +79,12 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 Constants.PROJECTBUILDER_SIGN_UP -> R.menu.project_builder_navigation_menu
                 else -> R.menu.bottom_navigation_menu
             }
-
-            setContentView(view)
             val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_graph) as NavHostFragment
             navController = navHostFragment.navController
             navController.setGraph(navGraphId)
 
             val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+            bottomNavigationView.menu.clear()
             bottomNavigationView.inflateMenu(menuResId)
             bottomNavigationView.setupWithNavController(navController)
 
@@ -104,12 +126,12 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         } else {
             val navGraphId = R.navigation.nav_graph
             val menuResId = R.menu.bottom_navigation_menu
-            setContentView(view)
             val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_graph) as NavHostFragment
             navController = navHostFragment.navController
             navController.setGraph(navGraphId)
 
             val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+            bottomNavigationView.menu.clear()
             bottomNavigationView.inflateMenu(menuResId)
             bottomNavigationView.setupWithNavController(navController)
 
@@ -147,40 +169,12 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         }
     }
 
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
-        drawer.closeDrawer(GravityCompat.END)
-
-        when(item.itemId) {
-            R.id.sign_in -> {
-                navController.navigate(R.id.signInFragment, null)
-            }
-            R.id.sign_up -> {
-                navController.navigate(R.id.signUpFragment, null)
-            }
-            R.id.logout -> {
-                logout()
-            }
-        }
-        return true
-    }
-
-    fun updateNavigationDrawer() {
-        if(sharedPreferences.getString(Constants.ACCESS_TOKEN, null) != null) {
-
-        } else {
-            binding.navView.setNavigationItemSelectedListener(this)
-            binding.navView.menu.findItem(R.id.logout).isVisible = false
-            binding.navView.menu.findItem(R.id.sign_in).isVisible = true
-            binding.navView.menu.findItem(R.id.sign_up).isVisible = true
-        }
-    }
-
     private fun logout() {
         sharedPreferences.edit {
             putString(Constants.ACCESS_TOKEN, null)
             putString(Constants.REFRESH_TOKEN, null)
         }
+        updateNavigation()
         binding.drawerLayout.closeDrawer(GravityCompat.END)
     }
 }
