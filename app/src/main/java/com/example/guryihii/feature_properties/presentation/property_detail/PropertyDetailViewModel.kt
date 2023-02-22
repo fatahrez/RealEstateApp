ae0c5e368e2e58e2a5e1c7e0afb80a79d0825c28
fatahrez <fertahrez@gmail.com>
2023-02-22 15:05:23 +0300

    successfully list property by agent

--- app/src/main/java/com/example/guryihii/feature_properties/presentation/property_detail/PropertyDetailViewModel.kt
+++ app/src/main/java/com/example/guryihii/feature_properties/presentation/property_detail/PropertyDetailViewModel.kt
@@ -5,6 +5,8 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.guryihii.core.util.ResultWrapper
 import com.example.guryihii.feature_properties.domain.usecases.GetPropertyDetails
+import com.example.guryihii.feature_properties.domain.usecases.PostPropertyListing
+import com.example.guryihii.feature_properties.presentation.property_detail.post_property_listing.PostPropertyListingState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -15,11 +17,15 @@ import javax.inject.Inject
 
 @HiltViewModel
 class PropertyDetailViewModel @Inject constructor(
-    private val getPropertyDetails: GetPropertyDetails
+    private val getPropertyDetails: GetPropertyDetails,
+    private val postPropertyListing: PostPropertyListing
 ): ViewModel() {
 
     private var _state = MutableStateFlow(PropertyDetailState())
     val state: StateFlow<PropertyDetailState> get() = _state
+
+    private var _propertyListingState = MutableStateFlow(PostPropertyListingState())
+    val propertyListingState: StateFlow<PostPropertyListingState> get() = _propertyListingState
 
     fun showPropertyDetail(slug: String) {
         viewModelScope.launch {
@@ -55,7 +61,41 @@ class PropertyDetailViewModel @Inject constructor(
                     }
                 }.launchIn(viewModelScope)
         }
+    }
 
+    fun postAgentPropertyListing(property: String) {
+        viewModelScope.launch {
+            postPropertyListing(property)
+                .collect { result ->
+                    when(result) {
+                        is ResultWrapper.Loading -> {
+                            _propertyListingState.value = propertyListingState.value.copy(
+                                isLoading = true
+                            )
+                        }
+                        is ResultWrapper.Success -> {
+                            _propertyListingState.value = propertyListingState.value.copy(
+                                isLoading = false,
+                                propertyListing = result.value ?: null
+                            )
+                        }
+                        is ResultWrapper.NetworkError -> {
+                            Log.e("TAG", "postAgentPropertyListing: network error")
+                            _propertyListingState.value = propertyListingState.value.copy(
+                                isLoading = false,
+                                propertyListing = null
+                            )
+                        }
+                        is ResultWrapper.GenericError -> {
+                            Log.e("TAG", "postAgentPropertyListing: ${result.error}")
+                            _propertyListingState.value = propertyListingState.value.copy(
+                                isLoading = false,
+                                propertyListing = null
+                            )
+                        }
+                    }
+                }
+        }
     }
 
 }
