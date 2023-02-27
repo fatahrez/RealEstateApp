180e21e21ef039e93e81d52456a0603f48699dc6
fatahrez <fertahrez@gmail.com>
2023-02-27 16:35:47 +0300

    finish adding DI

--- app/src/main/java/com/example/guryihii/feature_requests/di/RequestModule.kt
+++ app/src/main/java/com/example/guryihii/feature_requests/di/RequestModule.kt
@@ -1,5 +1,8 @@
 package com.example.guryihii.feature_requests.di
 
+import com.example.guryihii.core.util.Constants
+import com.example.guryihii.feature_requests.data.remote.RequestAPI
+import com.example.guryihii.feature_requests.data.repository.RequestRepositoryImpl
 import com.example.guryihii.feature_requests.domain.repository.RequestRepository
 import com.example.guryihii.feature_requests.domain.usecases.GetAllRequests
 import com.example.guryihii.feature_requests.domain.usecases.PostRequest
@@ -7,6 +10,10 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 import javax.inject.Singleton
 
 @Module
@@ -32,9 +39,20 @@ object RequestModule {
     @Provides
     @Singleton
     fun providesRequestRepository(
+        api: RequestAPI
+    ): RequestRepository {
+        return RequestRepositoryImpl(api)
+    }
 
-    ) {
-
+    @Provides
+    @Singleton
+    fun providesRequestAPI(okHttpClient: OkHttpClient): RequestAPI {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+            .create(RequestAPI::class.java)
     }
 
 }
