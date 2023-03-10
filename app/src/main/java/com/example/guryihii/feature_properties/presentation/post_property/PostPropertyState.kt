0a9b90b4d564da34249ddfb76deda0ebc9d11f0e
fatahrez <fertahrez@gmail.com>
2023-03-10 17:11:39 +0300

    fix quotes that are there when uploading property

--- app/src/main/java/com/example/guryihii/feature_properties/presentation/post_property/PostPropertyState.kt
+++ app/src/main/java/com/example/guryihii/feature_properties/presentation/post_property/PostPropertyState.kt
@@ -4,5 +4,6 @@ import com.example.guryihii.feature_properties.domain.model.Property
 
 data class PostPropertyState(
     val property: Property? = null,
-    val isLoading: Boolean = false
+    val isLoading: Boolean = false,
+    val error: String? = null
 )
