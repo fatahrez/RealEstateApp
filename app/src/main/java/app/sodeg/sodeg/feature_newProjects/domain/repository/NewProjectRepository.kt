269c634e6c8b34d1b47169acf457ae67c28218d0
fatahrez <fertahrez@gmail.com>
2023-03-12 10:17:10 +0300

    add repository implementation for post and update NewProject

--- app/src/main/java/app/sodeg/sodeg/feature_newProjects/domain/repository/NewProjectRepository.kt
+++ app/src/main/java/app/sodeg/sodeg/feature_newProjects/domain/repository/NewProjectRepository.kt
@@ -4,11 +4,49 @@ package app.sodeg.sodeg.feature_newProjects.domain.repository
 import app.sodeg.sodeg.core.util.ResultWrapper
 import app.sodeg.sodeg.feature_newProjects.domain.model.NewProject
 import kotlinx.coroutines.flow.Flow
+import okhttp3.MultipartBody
 
 interface NewProjectRepository {
 
     suspend fun getAllNewProjects(): Flow<ResultWrapper<List<NewProject>>>
 
     suspend fun getNewProjectDetails(slug: String): Flow<ResultWrapper<NewProject>>
+
+    suspend fun postNewProject(
+        name: MultipartBody.Part,
+        location: MultipartBody.Part,
+        description: MultipartBody.Part,
+        price: MultipartBody.Part,
+        bedrooms: Int,
+        bathrooms: Int,
+        squareFeet: Int,
+        country: MultipartBody.Part,
+        city: MultipartBody.Part,
+        constructionStatus: MultipartBody.Part,
+        completionDate: MultipartBody.Part,
+        propertyType: MultipartBody.Part,
+        coverPhoto: MultipartBody.Part,
+        photo1: MultipartBody.Part,
+        photo2: MultipartBody.Part
+    ): Flow<ResultWrapper<NewProject>>
+
+    suspend fun updateNewProject(
+        slug: String,
+        name: MultipartBody.Part,
+        location: MultipartBody.Part,
+        description: MultipartBody.Part,
+        price: MultipartBody.Part,
+        bedrooms: Int,
+        bathrooms: Int,
+        squareFeet: Int,
+        country: MultipartBody.Part,
+        city: MultipartBody.Part,
+        constructionStatus: MultipartBody.Part,
+        completionDate: MultipartBody.Part,
+        propertyType: MultipartBody.Part,
+        coverPhoto: MultipartBody.Part,
+        photo1: MultipartBody.Part,
+        photo2: MultipartBody.Part
+    ): Flow<ResultWrapper<NewProject>>
 
 }
