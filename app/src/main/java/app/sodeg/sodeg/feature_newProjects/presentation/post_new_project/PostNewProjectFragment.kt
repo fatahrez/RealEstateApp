d54c82bb61a82d794e28d0c445931e98143f5803
fatahrez <fertahrez@gmail.com>
2023-03-12 11:21:16 +0300

    add style for Post Property Fragment

--- app/src/main/java/app/sodeg/sodeg/feature_newProjects/presentation/post_new_project/PostNewProjectFragment.kt
+++ app/src/main/java/app/sodeg/sodeg/feature_newProjects/presentation/post_new_project/PostNewProjectFragment.kt
@@ -6,19 +6,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import app.sodeg.sodeg.databinding.FragmentNewProjectsBinding
+import app.sodeg.sodeg.databinding.FragmentPostNewProjectBinding
 
 
 class PostNewProjectFragment : Fragment() {
 
-    private var _binding: FragmentNewProjectsBinding? = null
-    private val binding: FragmentNewProjectsBinding get() = _binding!!
+    private var _binding: FragmentPostNewProjectBinding? = null
+    private val binding: FragmentPostNewProjectBinding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        _binding = FragmentNewProjectsBinding.inflate(inflater, container, false)
+        _binding = FragmentPostNewProjectBinding.inflate(inflater, container, false)
         return binding.root
     }
 
