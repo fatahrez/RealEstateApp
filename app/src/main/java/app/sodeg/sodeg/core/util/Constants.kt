8c82fdeb8bc8d21334d551f399694e93889e2366
Fatah <fertahrez@gmail.com>
2026-02-16 14:54:37 +0300

    solve build errors and revisit project

--- app/src/main/java/app/sodeg/sodeg/core/util/Constants.kt
+++ app/src/main/java/app/sodeg/sodeg/core/util/Constants.kt
@@ -1,13 +1,13 @@
 package app.sodeg.sodeg.core.util
 
 object Constants {
-    const val BASE_URL = "http://165.232.134.198/api/"
-    const val BASE_URL_IMAGE = "http://165.232.134.198"
+    const val BASE_URL = "http://10.0.2.2:8000/api/"
+    const val BASE_URL_IMAGE = "http://10.0.2.2:8000"
     const val REFRESH_TOKEN = "REFRESH_TOKEN"
     const val ACCESS_TOKEN = "ACCESS_TOKEN"
     const val INDIVIDUAL_SIGN_UP = "INDIVIDUAL"
     const val AGENT_SIGN_UP = "AGENT"
     const val SELLER_SIGN_UP = "SELLER"
     const val PROJECTBUILDER_SIGN_UP = "PROJECTBUILDER"
-    const val LOGIN_URL = "http://165.232.134.198/api/users/login/"
+    const val LOGIN_URL = "http://10.0.2.2:8000/api/users/login/"
 }
